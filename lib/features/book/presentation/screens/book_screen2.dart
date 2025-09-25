debb2825dabe462db896f2aa290896a37e3ef019
AbdulsemedM <abdumussema22@gmail.com>
2025-09-25 11:38:09 +0300

    feat(book): add book screen 2 with navigation and category handling
    - Add new book screen with improved layout and navigation
    - Implement back button functionality and category tap handling
    - Add new assets for book screen 2 including images and icons

---
+++ lib/features/book/presentation/screens/book_screen2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/book_content2.dart';
+
+class BookScreen2 extends StatelessWidget {
+  const BookScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookContent2(),
+    );
+  }
+}
